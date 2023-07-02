7fec71a28c9169101affe4e54e10d4d0c65640f7
Eric Lachance <ericlachance029@gmail.com>
2023-07-02 15:17:18 -0400

    Made a quick login example, there is also a word doc that breaks down the project requirements a little

---
+++ LoginTest/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace LoginTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.welcome = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(100, 91);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(55, 13);
+            this.usernameLabel.TabIndex = 0;
+            this.usernameLabel.Text = "Username";
+            this.usernameLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(102, 125);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(53, 13);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "Password";
+            // 
+            // welcome
+            // 
+            this.welcome.AutoSize = true;
+            this.welcome.Location = new System.Drawing.Point(100, 230);
+            this.welcome.Name = "welcome";
+            this.welcome.Size = new System.Drawing.Size(102, 13);
+            this.welcome.TabIndex = 2;
+            this.welcome.Text = "You have logged in!";
+            this.welcome.Visible = false;
+            this.welcome.Click += new System.EventHandler(this.welcome_Click);
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(162, 91);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.usernameTextBox.TabIndex = 3;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(162, 122);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordTextBox.TabIndex = 4;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(105, 187);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 5;
+            this.loginButton.Text = "Log In";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.welcome);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label welcome;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
+
---
+++ LoginTest/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LoginTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+
+            //Establishes connection
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=DESKTOP-VU3ANP7\\SQLEXPRESS01;Initial Catalog=PassTest;Integrated Security=true";
+            con.Open();
+
+            //Gets Name, Username and Password from DB, and compares them to textbox input
+            SqlCommand cmd = new SqlCommand("SELECT Name, UserName, Password FROM Login2 WHERE UserName = @username AND Password = @password", con);
+            //Assigning value from textBoxes to SQL parameter values
+            cmd.Parameters.AddWithValue("@username", usernameTextBox.Text);
+            cmd.Parameters.AddWithValue("@password", passwordTextBox.Text);
+
+            //Creating a table and filling it with info found in DB
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            //If no info was found in DB (therefore, table wouldn't have any rows), ignore this
+            //If at least 1 row found, execute below (print 'Name' from DB, matching username/password) 
+            if (dt.Rows.Count > 0)
+            {
+                string name = dt.Rows[0]["Name"].ToString();
+                MessageBox.Show("Login successful, welcome " + name);
+            }
+            else
+            {
+                MessageBox.Show("Invalid Login please check username and password");
+            }
+            con.Close();
+        }
+
+        private void welcome_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Username or Password is incorrect");
+        }
+    }
+}
